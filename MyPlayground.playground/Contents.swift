462720e6c545ed80872fbdf7fbbdf62dec6edc81
l374382803 <374382803@qq.com>
2016-01-11 15:04:45 +0800

    小项目
    初学

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,19 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+let num:Int = Int.max
+print(str)
+print("\(str)  hellow")
+var ma:Int?
+ma = 1000
+ma = 100
+ma = 0b1001
+ma = 0x1001
+ma = 0o1001
+print("hello")
+var st:String = "10000"
+var num1:Int = 100
+let string:String = "123446"
